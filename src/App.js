08576a1ed2ca0d031b4a69aaa954d4f77852a0a7
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-09-23 19:51:20 -0300

    feat:pre-jogo

--- src/App.js
+++ src/App.js
@@ -14,27 +14,43 @@ import forca6 from './assets/forca6.png'
 export default function App() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+    const [imagemForca, setImagemForca] = useState(forca0)
+    const [desabilitaAlfabeto, setDesabilitaAlfabeto] = useState(true)
+
+
+    function exibeLetraAlfabeto(letra) {
+
+        return (
+            // <button className="btn-letra" disabled={desabilitaAlfabeto}  data-identifier="letter" data-index={letra} key={letra} onClick={() => palpitaLetra(letra)}>
+             <button className="btn-letra" disabled={desabilitaAlfabeto}  data-identifier="letter" data-index={letra} key={letra}>
+                {letra}
+            </button>
+        )
+    }
+
     return (
         <div className="cenario">
             <div className="area-forca">
                 <div className="imagem-forca">
+                <img src={imagemForca} alt="" data-identifier="game-image" />
 
                 </div>
                 <div className="area-palavra">
                     <div >
-                        <button>Escolher Palavra</button>
+                        <button className="btn-escolher-palavra">Escolher Palavra</button>
                     </div>                    
                     
                 </div>
 
             </div>
             <div className="area-alfabeto">
+                {alfabeto.map((a) => exibeLetraAlfabeto(a))}
 
             </div>
             <div className="area-input">
                 <div>Já sei a palavra!</div>
-                <div><input /></div>
-                <div><button>Chutar</button></div>
+                <div><input  disabled={desabilitaAlfabeto} /></div>
+                <div><button className="btn-chutar" disabled={desabilitaAlfabeto}>Chutar</button></div>
 
             </div>
 
